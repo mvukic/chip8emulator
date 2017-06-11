6cd54f1e686ed761970012e03f1b7c6342d7bc81
Matija-Vukic <matija.vukic@hotmail.com>
2017-06-11 18:11:29 +0200

    disassembler improvements

--- src/main/kotlin/hr/mvukic/chip8emu/impl/OpcodeParser.kt
+++ src/main/kotlin/hr/mvukic/chip8emu/impl/OpcodeParser.kt
@@ -22,39 +22,53 @@ class OpcodeParser{
 
         when(hi.toPositiveInt() and 0xf0){
             0x00 -> when(lo.toPositiveInt()){
-                0xEE -> return Opcode(0,"$address","$higher$lower","RET")
-                0xE0 -> return Opcode(0,"$address","$higher$lower","CLS")
-                else -> return Opcode(0,"$address","$higher$lower","EXEC ${address.toHex()} OR DATA")
+                0xEE -> return Opcode(0,"$address","$higher$lower","ret")
+                0xE0 -> return Opcode(0,"$address","$higher$lower","cls")
+                else -> return Opcode(0,"$address","$higher$lower","exec ${address.toHex()} OR DATA")
             }
             0x10 -> {
                 val adr = (address and 0x0fff).toHex()
-                return Opcode(0,"$address","$higher$lower","JMP $adr")
+                return Opcode(0,"$address","$higher$lower","jmp #$adr")
             }
             0x20 -> {
                 val adr = (address and 0x0fff).toHex()
-                return Opcode(0,"$address","$higher$lower","CALL $adr")
+                return Opcode(0,"$address","$higher$lower","call $adr")
             }
             0x30 -> {
                 val value = lo.toPositiveInt().toHex()
                 val reg = (hi and 0x0f).toHex()
-                return Opcode(0,"$address","$higher$lower","seq V$reg #$value")
+                return Opcode(0,"$address","$higher$lower","seq r$reg #$value")
             }
             0x40 -> {
                 val value = lo.toPositiveInt().toHex()
                 val reg = (hi and 0x0f).toHex()
-                return Opcode(0,"$address","$higher$lower","sne V$reg #$value")
+                return Opcode(0,"$address","$higher$lower","sne r$reg #$value")
             }
             0x50 -> {
                 val reg1 = (hi and 0x0f).toHex()
                 val reg2 = (lo.toPositiveInt() shr 8).toHex()
-                return Opcode(0,"$address","$higher$lower","seq V$reg1 V$reg2")
+                return Opcode(0,"$address","$higher$lower","seq r$reg1 V$reg2")
             }
-            0x60 -> return Opcode(0,"$address","$higher$lower","with 6")
-            0x70 -> return Opcode(0,"$address","$higher$lower","with 7")
+            0x60 -> {
+                val value = lo.toPositiveInt().toHex()
+                val reg = (hi and 0x0f).toHex()
+                return Opcode(0,"$address","$higher$lower","mov #$value, r$reg")
+            }
+            0x70 -> {
+                val value = lo.toPositiveInt().toHex()
+                val reg = (hi and 0x0f).toHex()
+                return Opcode(0,"$address","$higher$lower","add #$value, r$reg")
+            }
             0x80 -> return Opcode(0,"$address","$higher$lower","with 8")
             0x90 -> return Opcode(0,"$address","$higher$lower","with 9")
-            0xA0 -> return Opcode(0,"$address","$higher$lower","with A")
-            0xB0 -> return Opcode(0,"$address","$higher$lower","with B")
+            0xA0 -> {
+                val adr = (address and 0x0fff).toHex()
+                return Opcode(0,"$address","$higher$lower","mov I, #$adr")
+            }
+            0xB0 -> {
+                val adr = (address and 0x0fff).toHex()
+                return Opcode(0,"$address","$higher$lower","jmp #($adr+[r0])")
+            }
             0xC0 -> return Opcode(0,"$address","$higher$lower","with C")
             0xD0 -> return Opcode(0,"$address","$higher$lower","with D")
             0xE0 -> return Opcode(0,"$address","$higher$lower","with E")
